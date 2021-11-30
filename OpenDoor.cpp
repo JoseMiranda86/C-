db07edfca94ff82c38dba5fe59dd21d8f0f11974
JoseMiranda86 <josemiranda_86@hotmail.com>
2021-11-30 20:13:40 +0900

    Finding pressure area

--- OpenDoor.cpp
+++ OpenDoor.cpp
@@ -27,3 +27,29 @@ void UOpenDoor::BeginPlay()
 	FindAudioComponent();
 
 }
+
+void UOpenDoor::FindpressurePlate()
+{
+	if(!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressureplates set!"), *GetOwner() -> GetName());
+	}
+}
+
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (TotalMassOfActors() > 50.f)
+	{
+		OpenDoor(DeltaTime);
+		DoorLastOpened = GetWorld() -> GetTimeSeconds();
+	}
+	else
+	{
+		if (GetWorld() -> GetTimeSeconds() - DoorLastOpened > DoorCloseDelay)
+		{
+			CloseDoor(DeltaTime);
+		}
+	}
+}
--- OpenDoor.h
+++ OpenDoor.h
@@ -4,3 +4,43 @@
 #include "Components/ActorComponent.h"
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
+	float TotalMassOfActors() const;
+	void FindAudioComponent();
+	void FindpressurePlate();
+
+	// Tracks wheter the sound has been played
+	bool OpenDoorSound = false;
+	bool CloseDoorSound = true;
+
+
+private:
+
+	float InitialYaw;
+	float CurrentYaw;
+
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate = nullptr;
+	
+};
