f3f0487cccb1a07eb27ff8576ab38697bc3cb541
JoseMiranda86 <josemiranda_86@hotmail.com>
2021-12-01 21:42:32 +0900

    Component on going. Open, Close functions

--- OpenDoor.cpp
+++ OpenDoor.cpp
@@ -5,17 +5,13 @@
 #include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
 
-// Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
-// Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
@@ -52,4 +48,29 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 			CloseDoor(DeltaTime);
 		}
 	}
+}
+
+void UOpenDoor::OpenDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, OpenAngle, DeltaTime * DoorOpenSpeed);
+	FRotator DoorRotation = GetOwner() -> GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner() -> SetActorRotation(DoorRotation);
+
+	CloseDoorSound = false;
+	if (!AudioComponent) {return;}
+	if (!OpenDoorSound)
+	{
+		AudioComponent -> Play();
+		OpenDoorSound = true;
+	}
+}
+
+void UOpenDoor::CloseDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
+	FRotator DoorRotation = GetOwner() -> GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner() -> SetActorRotation(DoorRotation);
+
 }
