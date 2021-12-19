35441556ff948ab10875fff0d227469651338bea
JoseMiranda86 <josemiranda_86@hotmail.com>
2021-12-19 17:56:03 +0900

    Building Base Pawn component

---
+++ Base_Pawn.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABasePawn();
+
+	void HandleDestruction();
+
+protected:
+	void RotateTurret(FVector LookAtTarget);
+	void Fire();	
