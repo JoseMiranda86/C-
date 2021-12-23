33d5ef68ee45080ce0f65104115707a54984afbe
JoseMiranda86 <josemiranda_86@hotmail.com>
2021-12-23 23:20:58 +0900

    Component files created, ongoing development

---
+++ Tower.cpp
@@ -0,0 +1,31 @@
+
+#include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
+void ATower::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (InFireRange())
+    {
+        RotateTurret(Tank -> GetActorLocation());
+    }
+}
+
+void ATower::HandleDestruction()
+{
+    Super::HandleDestruction();
+    Destroy();
+}
+
+
+void ATower::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+    GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
+}
