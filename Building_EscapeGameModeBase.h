d8242877c45f70abfeab9df424e9dc6c8c983326
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-01-05 22:46:30 +0900

    Component Game Mode building Escape

---
+++ Building_EscapeGameModeBase.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Building_EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_API ABuilding_EscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
